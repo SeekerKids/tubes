ae205a5202b639c04dfc8590a9886b97b51f148b
SeekerKids <155258191+SeekerKids@users.noreply.github.com>
2024-01-01 12:20:20 +0700

    Update1 BankSoal.h

--- BankSoal.h
+++ BankSoal.h
@@ -2,11 +2,13 @@
 #define BANKSOAL_H_INCLUDED
 
 #include <iostream>
+#include <vector>
 
 #define firstSoal(L) L.firstSoal
 #define nextSoal(P) P->nextSoal
 #define infoSoal(P) P->infoSoal
 #define nilSoal NULL
+
 
 using namespace std;
 
@@ -38,5 +40,10 @@ AdrSoal allocateSoal(InfoSoal x);
 void insertLastSoal(ListSoal &L, AdrSoal P);
 int countSoal(ListSoal L);
 void displayAllSoal(ListSoal L);
+void addSoal(ListSoal &L);
+AdrSoal findSoalByIndex(const ListSoal L, int index);
+void editSoal(ListSoal &L);
+void displayTopQuestions(/*ListQuiz L,*/ ListSoal &L);
+
 
 #endif // BANKSOAL_H_INCLUDED
