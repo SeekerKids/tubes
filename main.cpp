f473594cb6725c1135269f7d977e7013d93f44d3
SeekerKids <155258191+SeekerKids@users.noreply.github.com>
2024-01-01 19:55:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 void DummyData(ListUser &userList, ListSoal &bankSoal) {
     // Dummy Users
-    UserInfo user1 = {"user1", "pass1", false};
-    UserInfo user2 = {"user2", "pass2", false};
-    UserInfo user3 = {"user3", "pass3", false};
-    UserInfo user4 = {"admin", "admin", true};
+    UserInfo user4 = {"admin", "admin", true, -1};   // Admin user with score
+    UserInfo user1 = {"user1", "pass1", false, 20};  // Score initialized to 100
+    UserInfo user2 = {"user2", "pass2", false, 50;  // Score initialized to 150
+    UserInfo user3 = {"user3", "pass3", false, 30};  // Score initialized to 120
 
     AddressUser node1 = allocateUser(user1);
     AddressUser node2 = allocateUser(user2);
@@ -42,6 +42,7 @@ void DummyData(ListUser &userList, ListSoal &bankSoal) {
     insertLastSoal(bankSoal, question1);
     insertLastSoal(bankSoal, question2);
     insertLastSoal(bankSoal, question3);
+
 }
 
 int main() {
@@ -51,10 +52,12 @@ int main() {
     createUserList(userList);
     createSoalList(bankSoal);
 
+
     DummyData(userList, bankSoal);
 
     int choice, choiceLogin;
     bool loggedIn = false;
+
     string loggedInUsername;
     string username, password;
 
@@ -71,6 +74,7 @@ int main() {
         cout << "1. Buat Akun" << endl;
         cout << "2. Login" << endl;
         cout << "3. Tampilkan Daftar Peserta" << endl;
+        cout << "4. Test" << endl;
         cout << "0. Keluar Aplikasi" << endl;
 
         cout << "Pilihan Anda: ";
@@ -107,6 +111,10 @@ int main() {
             break;
         case 3 :
             showAllUser(userList);
+            //SortDescending(userList);
+            break;
+        case 4:
+            countUser(userList);
             break;
         case 0:
             cout << "Terima kasih telah menggunakan aplikasi!" << endl;
@@ -141,8 +149,11 @@ int main() {
                 switch (choice) {
                     case 1:
                         //Mulai Quis;
+                        //Quiz(userList,bankSoal);
                         break;
                     case 2:
+                        SortDescending(userList);
+                        showSkor(userList);
                         //nampilkan daftar peserrta beserta skor;
                         break;
                     case 3:
@@ -177,15 +188,19 @@ int main() {
                 cin >> choice;
 
                 switch (choice) {
-                    cout << "ewe" << endl;
+                    //cout << "ewe" << endl; // nggak keluar
                     case 1:
+                        addSoal(bankSoal);
                         // Kode untuk tambah soal quiz (insert first)
                         break;
                     case 2:
+                        displayAllSoal(bankSoal);
                         // Kode untuk edit soal(show dlu semua soalnya
+                        editSoal(bankSoal);
                         break;
                     case 3:
                         // Kode untuk hapus soal quiz & kunci (delete last)
+                        // kasih disconet klo udah ada relasinya
                         break;
                     case 4:
                         //displayTopQuestions(bankSoal);
