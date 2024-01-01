6fe2461c121e978c7be59c0feb9a19b32bc56821
SeekerKids <155258191+SeekerKids@users.noreply.github.com>
2024-01-01 12:18:43 +0700

    Update User.h

--- User.h
+++ User.h
@@ -9,6 +9,8 @@
 #define prevUser(P) P->prevUser
 #define infoUser(P) P->infoUser
 #define nilUser NULL
+#include "BankSoal.h"
+
 
 using namespace std;
 
@@ -16,6 +18,7 @@ struct User {
     string username;
     string password;
     bool status;
+    int skor;
 };
 
 typedef struct ElementUser *AddressUser;
@@ -32,19 +35,16 @@ struct ListUser {
 };
 
 void createUserList(ListUser &L);
-
 AddressUser allocateUser(UserInfo x);
-
 void insertLastUser(ListUser &L, AddressUser P);
-
 AddressUser findUserByUsername(const ListUser &userList, const string &username);
-
+void deleteLastUser(ListUser &L, AddressUser &P);
 bool checkUsername(ListUser L, string name);
-
 void registerUser(ListUser &L);
-
 bool loginUser(ListUser L, string username, string password);
-
 void showAllUser(ListUser& L);
-
+void displayParticipantScores(ListUser L);
+int countUser(ListUser L);
+void SortDescending(ListUser L);
+void Quiz(ListUser &LU, ListSoal &LS);
 #endif // USER_H_INCLUDED
